57ae47920fe5b80916c7653cca866ee46e5be8d1
muratcan.bur <muratcan.bur@trendyol.com>
2023-03-20 21:28:39 +0300

    fix #3 | add Hilt support

---
+++ app/src/main/java/co/icanteach/android/deeplinktester/DeepLinkTesterApplication.kt
@@ -0,0 +1,7 @@
+package co.icanteach.android.deeplinktester
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DeepLinkTesterApplication : Application()
--- app/src/main/java/co/icanteach/android/deeplinktester/MainActivity.kt
+++ app/src/main/java/co/icanteach/android/deeplinktester/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import co.icanteach.android.deeplinktester.ui.theme.DeeplinkTesterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
